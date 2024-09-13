8e229d142bf5edc40e104fc8623f8265a6c3c429
Andrew Hartmann <a.r.hartmann@wustl.edu>
2024-09-13 11:43:08 -0500

    Added temporary character and ground sprites, created player gameobject that can move left and right and jump, and created ground prefab

---
+++ Assets/Code/PlayerContoller.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code
+{
+    public class PlayerContoller : MonoBehaviour
+    {
+        //Outlet
+        Rigidbody2D rb;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Move player left (A key)
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(Vector2.left * 18f * Time.deltaTime, ForceMode2D.Impulse);
+            }
+
+            //Move player right (D key)
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(Vector2.right * 18f * Time.deltaTime, ForceMode2D.Impulse);
+            }
+
+            //Jump
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+}
