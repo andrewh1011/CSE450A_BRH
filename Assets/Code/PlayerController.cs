5874bf3e86172227f2070e44a077a8e72b117ac0
bhat.k <55767235+kibh11@users.noreply.github.com>
2024-09-19 03:37:15 -0500

    Added Successively Powerful Triple Jump - Kiran

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -2,41 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Code
-{
-    public class PlayerController : MonoBehaviour
-    {
+namespace Code {
+    public class PlayerController : MonoBehaviour {
         //Outlet
         Rigidbody2D _rigidbody;
         public Transform aimPivot;
         public GameObject projectilePrefab;
 
+        // State Tracking
+        public int jumpsLeft;
 
         // Start is called before the first frame update
-        void Start()
-        {
+        void Start() {
             _rigidbody = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
-        void Update()
-        {
+        void Update() {
             //Move player left (A key)
-            if (Input.GetKey(KeyCode.A))
-            {
+            if (Input.GetKey(KeyCode.A)) {
                 _rigidbody.AddForce(Vector2.left * 18f * Time.deltaTime, ForceMode2D.Impulse);
             }
 
             //Move player right (D key)
-            if (Input.GetKey(KeyCode.D))
-            {
+            if (Input.GetKey(KeyCode.D)) {
                 _rigidbody.AddForce(Vector2.right * 18f * Time.deltaTime, ForceMode2D.Impulse);
             }
 
             //Jump
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _rigidbody.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                if (jumpsLeft == 3) {
+                    jumpsLeft--;
+                    _rigidbody.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+                }
+                else if (jumpsLeft == 2) {
+                    jumpsLeft--;
+                    _rigidbody.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+                }
+                else if (jumpsLeft == 1) {
+                    jumpsLeft--;
+                    _rigidbody.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
+                }
             }
 
             //Aim toward mouse
@@ -50,13 +56,25 @@ namespace Code
             aimPivot.rotation = Quaternion.Euler(0, 0, angleToMouse);
 
             //Shoot
-            if (Input.GetMouseButtonDown(0))
-            {
+            if (Input.GetMouseButtonDown(0)) {
                 GameObject newProjectile = Instantiate(projectilePrefab);
                 newProjectile.transform.position = transform.position;
                 newProjectile.transform.rotation = aimPivot.rotation;
             }
         }
-    }
 
+        void OnCollisionStay2D(Collision2D other) {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 1.125f);
+
+                for (int i = 0; i < hits.Length; i++) {
+                    RaycastHit2D hit = hits[i];
+
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+                        jumpsLeft = 3;
+                    }
+                }
+            }
+        }
+    }
 }
