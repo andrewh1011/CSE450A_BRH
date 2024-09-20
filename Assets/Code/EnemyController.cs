3727a5650d9c835d90c26ed692157ba8e9897797
Andrew Hartmann <a.r.hartmann@wustl.edu>
2024-09-20 12:27:24 -0500

    Added enemy prefabs that despawn when colliding with a player projectile and reset the scene when colliding with the player

---
+++ Assets/Code/EnemyController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Code
+{
+    public class EnemyController : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            //Reload the scene when colliding with player
+            if (collision.gameObject.GetComponent<PlayerController>())
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+            // Kill the enemy if colliding with the player projectile
+            if (collision.gameObject.GetComponent<Projectile>())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
