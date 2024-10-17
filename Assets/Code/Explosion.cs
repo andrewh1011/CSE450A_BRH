3703eddbaf6c94c5a29f1b23c190526b6800e690
Andrew Hartmann <a.r.hartmann@wustl.edu>
2024-10-17 18:16:18 -0500

    Explosion damage added - explosion collider gets larger with animation, and kills enemies

---
+++ Assets/Code/Explosion.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            if (collision.gameObject.tag == "Head")
+            {
+                Destroy(collision.gameObject.transform.parent.gameObject);
+            }
+            else
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
