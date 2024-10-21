35970656fc2c43d9e56bc34cd4babbf1a6a57d40
bhat.k <55767235+kibh11@users.noreply.github.com>
2024-10-21 17:13:34 -0500

    Expanded tilemap much further, changed arrow spawning so that arrow can reach player closer to level, removed momentum conservation on dash

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -102,15 +102,16 @@ namespace Code {
 
                 // End dash
                 if (dashTimer <= 0) {
-                    if (dashDirection == DashDirection.Left)
-                    {
-                        _rigidbody.AddForce(Vector2.left * 18f * Time.deltaTime, ForceMode2D.Impulse);
-                    }
-                    else if (dashDirection == DashDirection.Right)
-                    {
-                        _rigidbody.AddForce(Vector2.right * 18f * Time.deltaTime, ForceMode2D.Impulse);
-                    }
-                    dashesLeft--;
+                    _rigidbody.velocity = Vector2.zero;
+                    //if (dashDirection == DashDirection.Left)
+                    //{
+                    //    _rigidbody.AddForce(Vector2.left * 18f * Time.deltaTime, ForceMode2D.Impulse);
+                    //}
+                    //else if (dashDirection == DashDirection.Right)
+                    //{
+                    //    _rigidbody.AddForce(Vector2.right * 18f * Time.deltaTime, ForceMode2D.Impulse);
+                    //}
+                    //dashesLeft--;
                 }
             }
 
