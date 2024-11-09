32e25980dba93dfc5a6d5bccf5b10e2d85dfa0c3
bhat.k <55767235+kibh11@users.noreply.github.com>
2024-11-09 01:17:43 -0600

    added intro camera sequence with zoom and pan into character from endpoint of level

---
+++ Assets/Code/IntroSequence.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace Code {
+    public class IntroSequence : MonoBehaviour {
+        public CinemachineVirtualCamera virtualCamera;
+        public CinemachineTrackedDolly dolly;
+        public Transform player;
+        public float panDuration;
+        public float delayDuration;
+        public float zoomDuration;
+        public float panTimer = 0;
+        public float delayTimer = 0;
+        public float zoomTimer = 0;
+        public bool panComplete;
+        public bool delayComplete;
+        public bool zoomComplete;
+        public float startLensSize;
+        public float endLensSize;
+
+        private void Start() {
+           dolly = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+           dolly.m_PathPosition = 0;
+        }
+
+        private void Update() {
+            if (!delayComplete) {
+                delayTimer += Time.deltaTime;
+                if (delayTimer >= delayDuration) {
+                    delayComplete = true;
+                }
+                return;
+            }
+
+            if (!panComplete) {
+                panTimer += Time.deltaTime;
+                float progress = panTimer / panDuration;
+
+                dolly.m_PathPosition = progress * 123f;
+
+                if (progress >= 1) {
+                    panComplete = true;
+                    virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>().enabled = false;
+                    var transposer = virtualCamera.AddCinemachineComponent<CinemachineFramingTransposer>();
+                    transposer.enabled = true;
+                    virtualCamera.Follow = player;
+                }
+            }
+
+            if (!zoomComplete) {
+                zoomTimer += Time.deltaTime;
+                float zoomProgress = zoomTimer / zoomDuration;
+                virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(startLensSize, endLensSize, Mathf.SmoothStep(0, 1, zoomProgress));
+
+                if (zoomProgress >= 1) {
+                    zoomComplete = true;
+                    virtualCamera.m_Lens.OrthographicSize = endLensSize;
+                }
+            }
+        }
+    }
+}
