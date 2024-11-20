2493f3858062f48a0fbd092cab879e0ef32fee5e
bhat.k <55767235+kibh11@users.noreply.github.com>
2024-11-20 17:15:51 -0600

    Added tutorial message

---
+++ Assets/Code/TutorialMessage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code {
+    public class TutorialMessage : MonoBehaviour {
+        public Sprite[] letterSprites = new Sprite[26];
+
+        public GameObject[] letterObjects;
+
+        private string msg = "use a and d to move left and right   spacebar will allow you to jump or dash if you have already jumped   use the cursor to aim and shoot your weapon at enemies   collect powerups to upgrade your weapon   beware of level specific challenges   reach the castle to move on";
+
+        void Start() {
+            letterObjects = new GameObject[msg.Length];
+
+            for (int i = 0; i < msg.Length; i++) {
+                letterObjects[i] = new GameObject("Letter_" + i);
+                letterObjects[i].transform.parent = transform;
+
+                letterObjects[i].transform.localPosition = new Vector3(i * 1.0f, 0, 0);
+
+                SpriteRenderer spriteRenderer = letterObjects[i].AddComponent<SpriteRenderer>();
+
+                char letter = char.ToLower(msg[i]);
+                if (letter >= 'a' && letter <= 'z') {
+                    int spriteIndex = letter - 'a';
+                    spriteRenderer.sprite = letterSprites[spriteIndex];
+                } else if (letter == ' ') {
+                    spriteRenderer.sprite = null;
+                }
+            }
+        }
+    }
+}
